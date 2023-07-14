0ac53910820707ca78a3630637007096d543c448
Anatoly <kolganovpix@yandex.ru>
2023-07-14 23:14:32 +0500

    Добавляет &nbsp;

--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -19,7 +19,7 @@ function PlaceCard(): JSX.Element {
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
             <b className="place-card__price-value">€120</b>
-            <span className="place-card__price-text">/&nbsp;night</span>
+            <span className="place-card__price-text">&nbsp;/&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
             <svg className="place-card__bookmark-icon" width={18} height={19}>
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -91,7 +91,7 @@ function MainPage({ placesCount }: MainScreenProps): JSX.Element {
                 {placesCount} places to stay in Amsterdam
               </b>
               <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by </span>
+                <span className="places__sorting-caption">Sort by&nbsp;</span>
                 <span className="places__sorting-type" tabIndex={0}>
                   Popular
                   <svg className="places__sorting-arrow" width={7} height={4}>
