89099cd7b4b90e46a8dd5de3ea462f78de041401
Anatoly <kolganovpix@yandex.ru>
2023-07-18 23:45:02 +0500

    Добавит AppRoute в Page404

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,10 +1,12 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 
 function Page404(): JSX.Element {
   return(
     <>
       <h1>404. Page not found</h1>
-      <Link to="/">Вернуться на главную</Link>
+      <Link to={AppRoute.Root}>Вернуться на главную</Link>
     </>
   );
 }
