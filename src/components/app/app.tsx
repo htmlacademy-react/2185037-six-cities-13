450f1b7efefe7c8a21092b972cc7d347aa8757aa
Anatoly <kolganovpix@yandex.ru>
2023-07-18 23:43:11 +0500

    Добавит маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainPage from '../../pages/main';
+import FavoritesPage from '../../pages/favorites';
+import LoginPage from '../../pages/login';
+import OfferPage from '../../pages/offer';
+import Page404 from '../../pages/not-found';
 
 type AppScreenProps = {
   placesCount: number;
@@ -6,7 +12,30 @@ type AppScreenProps = {
 
 function App({placesCount}: AppScreenProps): JSX.Element {
   return (
-    <MainPage placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage placesCount={placesCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path={AppRoute.NotFound}
+          element={<Page404 />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,11 @@
 export const Setting = {
   PlacesCount: 5
 };
+
+export enum AppRoute {
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer',
+  Root = '/',
+  NotFound = '*'
+}
