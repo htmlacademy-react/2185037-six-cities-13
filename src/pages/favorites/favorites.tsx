4a1eda069e672ff69b2e8a6191606cd5a6aa7a84
Anatoly <kolganovpix@yandex.ru>
2023-07-25 13:15:45 +0500

    Добавит стейт, новые файлы, список предложений

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-import Logo from '../../components/logo';
+import Header from '../../components/header';
 
 function FavoritesPage(): JSX.Element {
   return (
@@ -7,36 +7,7 @@ function FavoritesPage(): JSX.Element {
       <Helmet>
         <title>6 sities: favorites</title>
       </Helmet>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
           <section className="favorites">
--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -1,5 +1,5 @@
 import { Helmet } from 'react-helmet-async';
-import Logo from '../../components/logo';
+import Header from '../../components/header';
 
 function OfferPage(): JSX.Element {
   return (
@@ -7,36 +7,7 @@ function OfferPage(): JSX.Element {
       <Helmet>
         <title>6 sities: offer</title>
       </Helmet>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="page__main page__main--offer">
         <section className="offer">
           <div className="offer__gallery-container container">
