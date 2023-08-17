3838cc8f323df9885e735fcec4dec5a74aceeb67
Anatoly <kolganovpix@yandex.ru>
2023-08-17 19:49:33 +0500

    Добавит useEffect и setView

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -13,15 +13,25 @@ type MapProps = {
   selectedOfferId: OfferPreview['id'] | undefined;
 };
 
-const getMarkerIcon = (url: string) => new Icon({
-  iconUrl: url,
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-});
+const getMarkerIcon = (url: string) =>
+  new Icon({
+    iconUrl: url,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
 
 function Map({ block, city, offers, selectedOfferId }: MapProps): JSX.Element {
   const mapRef = useRef(null);
   const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      map.setView(
+        [city.location.latitude, city.location.longitude],
+        city.location.zoom
+      );
+    }
+  }, [map, city]);
 
   useEffect(() => {
     if (map) {
@@ -34,7 +44,9 @@ function Map({ block, city, offers, selectedOfferId }: MapProps): JSX.Element {
 
         marker
           .setIcon(
-            offer.id === selectedOfferId ? getMarkerIcon(URL_MARKER_CURRENT) : getMarkerIcon(URL_MARKER_DEFAULT)
+            offer.id === selectedOfferId
+              ? getMarkerIcon(URL_MARKER_CURRENT)
+              : getMarkerIcon(URL_MARKER_DEFAULT)
           )
           .addTo(markerLayer);
       });
@@ -54,7 +66,7 @@ function Map({ block, city, offers, selectedOfferId }: MapProps): JSX.Element {
         width: '100%',
         minHeight: '500px',
         maxWidth: '1140px',
-        margin: '0 auto 50px'
+        margin: '0 auto 50px',
       }}
     />
   );
