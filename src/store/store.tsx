fe4a88cc622f7e27a1a5dfead0ca4f428325a1f5
Anatoly <kolganovpix@yandex.ru>
2023-08-24 19:42:34 +0500

    Подключим redux-thunk

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,10 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import offerSlice from './offer-slice';
+import { createAPI } from '../services/api';
+
+export const api = createAPI();
 
 const store = configureStore({
   reducer: {
     offers: offerSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
